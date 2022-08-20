58eb7f59dfc59456350e6767ba4df277aa421be9
jwong1209 <jwong1209qaz@gmail.com>
2022-08-19 18:07:44 -0700

    Added prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Jason Wong on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var Background: UIView!
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBAction func ChangeColorButton(_ sender: Any) {
+        print("Hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func ChangeTextButton(_ sender: Any) {
+        let newText = TextField.text
+        if (newText != nil && newText != "") {
+            TextLabel.text = newText
+        }
+        else if (newText == "" || newText == nil) {
+            TextLabel.text = "Hello From Jason"
+        }
+        TextField.resignFirstResponder()
+    }
+    
+    @IBAction func GoodbyeButton(_ sender: Any) {
+        TextLabel.text = "Goodbye 👋"
+    }
+    
+    @IBAction func ChangeBackgroundColor(_ sender: Any) {
+        Background.backgroundColor = UIColor.orange
+    }
+}
+
